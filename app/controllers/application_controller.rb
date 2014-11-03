8a162ddb822cfda857f2a8dd93a861335e878875
Josh Cheek <josh.cheek@gmail.com>
2014-11-03 15:04:35 -0700

    Authenticate with jsl-identity

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,29 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  Deject self, :user_repository
+
+  before_action :require_login
+  before_action :require_invitation_or_admin
+
+  helper_method :current_user
+  def current_user
+    @current_user ||= user_repository.find session[:user_id]
+  end
+
+  private
+
+  def require_login
+    current_user
+  rescue Jsl::Identity::ResourceNotFound
+    requested_url = Rack::Request.new(request.env).url
+    redirect_to user_repository.login_url(requested_url)
+  end
+
+  def require_invitation_or_admin
+    return if current_user.invited? || current_user.admin?
+    render text:   'An invitation is required to visit this site',
+           status: :forbidden
+  end
 end
---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+require 'jsl/identity/test'
+
+RSpec.describe ApplicationController, type: :controller do
+  controller ApplicationController do
+    def index
+      render text: current_user.name
+    end
+  end
+
+  let(:username)        { 'THE USERS NAME' }
+  let(:user)            { Jsl::Identity::Mock::User.factory name: username }
+  let(:user_repository) { Jsl::Identity::Mock::UserRepository.new }
+
+  before do
+    user_repository.will_find(user)
+    controller.with_user_repository user_repository
+  end
+
+  specify "Users must exist" do
+    user_repository.will_have_login_url('http://example.com/login/path')
+                   .will_find(Jsl::Identity::ResourceNotFound.new(user.class, 'some-url'))
+    get :index
+    expect(response).to be_a_redirect
+    expect(response.location).to eq 'http://example.com/login/path'
+  end
+
+  specify "Users must be invited" do
+    user.is_not_invited!
+    get :index
+    expect(response).to be_forbidden
+    expect(response.body).to_not include username
+
+    user.is_invited!
+    get :index
+    expect(response).to be_ok
+    expect(response.body).to include username
+  end
+
+  specify 'Admin do not need to be invited' do
+    user.is_not_invited!.is_admin = true
+    get :index
+    expect(response).to be_ok
+    expect(response.body).to include username
+  end
+end
