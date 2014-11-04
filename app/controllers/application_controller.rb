dd351f91c9eb4c9602068909ebc02504b70c1225
Jeff Casimir <jeff@casimircreative.com>
2014-11-04 14:15:50 -0700

    See what happens when there is no session_id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,7 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user
   def current_user
-    if session[:user_id]
-      @current_user ||= user_repository.find session[:user_id]
-    end
+    @current_user ||= user_repository.find session[:user_id]
   end
 
   helper_method :current_person
