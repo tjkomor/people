0d3ee0ce158f7766032f89e53ce136bacbe5b1f2
Jeff Casimir <jeff@casimircreative.com>
2014-11-04 14:07:50 -0700

    Only look for a User if the user_id is set

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,9 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user
   def current_user
-    @current_user ||= user_repository.find session[:user_id]
+    if session[:user_id]
+      @current_user ||= user_repository.find session[:user_id]
+    end
   end
 
   helper_method :current_person
