b3276173be5b6f58339c2334bed562c5c6981187
Jeff Casimir <jeff@casimircreative.com>
2014-11-06 15:05:23 -0700

    Counting apparently starts at 1 in Postgres

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,12 +3,13 @@ class Location < ActiveRecord::Base
   has_many :people, :through => :location_interests
 
   validates_uniqueness_of :name
+  validates_presence_of :name
 
   def self.names
     order(:name).pluck(:name)
   end
 
   def self.active
-    order("split_part(name, ', ', 1)")
+    order("split_part(name, ', ', 2)")
   end
 end
