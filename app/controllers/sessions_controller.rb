574c44f3ca4a08044e08b9e184a446d1a3d3d347
worace <horace.d.williams@gmail.com>
2015-04-08 14:07:50 -0600

    First steps toward using turing_auth gem for user auth
    Import local copy of the gem and start applying
    it in appropriate places

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,35 @@
+class SessionsController < ApplicationController
+  def new
+    if current_user
+      redirect_to subdomains_path
+    else
+      redirect_to '/auth/github'
+    end
+  end
+
+  def create
+    auth = request.env["omniauth.auth"]
+    user_info = {github_id: auth.uid, github_token: auth.credentials.token,
+                 github_name: auth.info.nickname, email: auth.info.email}
+    user = TuringAuth::User.new(user_info)
+    if user.valid? && user.turing_member?
+      @current_user = user
+      session[:current_user] = @current_user.as_json
+      redirect_to root_path
+    else
+      flash[:error] = "Sorry, only members of the Turing github organization can do that."
+      redirect_to root_path
+    end
+  end
+
+  def failure
+    Rails.logger.error("Oauth failed...#{params[:message]}")
+    flash[:notice] = params[:message]
+    redirect_to root_path
+  end
+
+  def destroy
+    session[:current_user] = nil
+    redirect_to root_path
+  end
+end
---
+++ config/initializers/turing_auth.rb
@@ -0,0 +1,3 @@
+TuringAuth.client_id = ENV['NAMER_GITHUB_ID']
+TuringAuth.client_secret = ENV['NAMER_GITHUB_SECRET']
+TuringAuth.init! #init omniauth extensions
