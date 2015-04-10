b6b8252cce77ff02af18ba9fcb8b8bbb31e558f9
worace <horace.d.williams@gmail.com>
2015-04-10 11:47:47 -0600

    Reset to default session configs to fix Oauth CSRF issue
    We aren't actually using the domain: :all session option
    for the People app (in theory we could try to set sessions
    across all the *.turing.io domains but we don't really need
    this for People).
    
    Since this seems to be causing the session CSRF issue for omniauth,
    let's just revert to normal settings.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store,
-  key:    '_turing_session'
-#domain: 'my_app.com
+Rails.application.config.session_store :cookie_store, key: '_people_session'
--- config/initializers/turing_auth.rb
+++ config/initializers/turing_auth.rb
@@ -1,10 +1,4 @@
 TuringAuth.admin_token = ENV["TURING_AUTH_ADMIN_TOKEN"]
 TuringAuth.client_id = ENV['PEOPLE_GITHUB_ID']
 TuringAuth.client_secret = ENV['PEOPLE_GITHUB_SECRET']
-#TuringAuth.init! #init omniauth extensions
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :developer unless Rails.env.production?
-  provider :github, TuringAuth.client_id, TuringAuth.client_secret, provider_ignores_state: true
-end
-
+TuringAuth.init! #init omniauth extensions
