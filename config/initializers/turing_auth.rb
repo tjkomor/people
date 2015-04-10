87f933e337c7e25b371dc4283b03027ca491cf29
worace <horace.d.williams@gmail.com>
2015-04-09 19:18:25 -0600

    attempting a workaround for csrf auth issue wiht github

--- config/initializers/turing_auth.rb
+++ config/initializers/turing_auth.rb
@@ -1,4 +1,10 @@
 TuringAuth.admin_token = ENV["TURING_AUTH_ADMIN_TOKEN"]
 TuringAuth.client_id = ENV['PEOPLE_GITHUB_ID']
 TuringAuth.client_secret = ENV['PEOPLE_GITHUB_SECRET']
-TuringAuth.init! #init omniauth extensions
+#TuringAuth.init! #init omniauth extensions
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :developer unless Rails.env.production?
+  provider :github, TuringAuth.client_id, TuringAuth.client_secret, provider_ignores_state: true
+end
+
