e3f8301ff170b07013b0205828d6b948d5e2146a
worace <horace.d.williams@gmail.com>
2015-04-09 18:34:34 -0600

    use new config option to allow less invasive oauth

--- config/initializers/turing_auth.rb
+++ config/initializers/turing_auth.rb
@@ -1,3 +1,4 @@
+TuringAuth.admin_token = ENV["TURING_AUTH_ADMIN_TOKEN"]
 TuringAuth.client_id = ENV['PEOPLE_GITHUB_ID']
 TuringAuth.client_secret = ENV['PEOPLE_GITHUB_SECRET']
 TuringAuth.init! #init omniauth extensions
