52b7fde04cb7d36d5ebfaae58314709463d34457
worace <horace.d.williams@gmail.com>
2015-04-09 19:27:53 -0600

    what about no setting?

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :cookie_store,
-  key:    '_turing_session',
-  domain: "turing-people-staging.herokuapp.com"
+  key:    '_turing_session'
 #domain: 'my_app.com
