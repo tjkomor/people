582c78fe8c99f2bcab4ec5b9ccaf92fb22ff22da
novohispano <novohispano@gmail.com>
2015-07-13 14:17:16 -0600

    Clean router to improve consistency

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
   resources :people, :locations, :projects
+
   resources :location_interests do
     collection do
-      post :build_location_and, :to => "location_interests#build_location_and_interest"
+      post :build_location_and, to: 'location_interests#build_location_and_interest'
     end
   end
+
   resource  :dashboard
   resource  :resume
   root      'people#index'
 
-  get "/auth/:provider/callback" => "sessions#create"
-  get "/auth/failure" => "sessions#failure", as: :login_failure
-  get '/login' => 'sessions#new', :as => :login
-  get '/logout' => 'sessions#destroy', :as => :logout
+  get '/auth/:provider/callback' => 'sessions#create'
+  get '/auth/failure'            => 'sessions#failure', as: :login_failure
+  get '/login'                   => 'sessions#new',     as: :login
+  get '/logout'                  => 'sessions#destroy', as: :logout
 end
